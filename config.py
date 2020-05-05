d6335f39866c31c0894997e1f77ece9db955c491
mercy-shii <wemercy525@gmail.com>
2020-05-06 00:44:05 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
-
+import os
 
 class Config:
     '''
     General configuration parent class
     '''
+    
+    SECRET_KEY ='shii-mercy'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mercy:shii@localhost/mercypitches'
-     
-    SECRET_KEY ='secret key'
 
 
 
@@ -18,7 +22,20 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class TestConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    ''' 
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mercy:shii@localhost/mercypitches_test'   
+     
+    
 
 
 class DevConfig(Config):
@@ -28,10 +45,12 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mercy:shii@localhost/mercypitches'
 
     DEBUG = True
 
 config_options = {
     'development' :DevConfig,
-    'production' :ProdConfig
+    'production' :ProdConfig,
+    'test':TestConfig
 }    
