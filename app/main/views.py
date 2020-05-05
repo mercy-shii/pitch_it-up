059d0656d163f05c3ac36b0081f984afb09ae05e
mercy-shii <wemercy525@gmail.com>
2020-05-05 20:05:54 +0300

    comment functionality

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,redirect,url_for
 from . import main
 from flask_login import login_required,current_user
-from .forms import PitchForm
+from .forms import PitchForm,CommentForm
 from .. import db
 from ..models import Pitches,User,Comments
 
@@ -37,73 +37,44 @@ def new_pitch():
 
     return render_template('pitch.html',pitch_entry= form)
 
-@main.route('/categories/<cate>')
+@main.route('/categories/<cate>',methods = ['GET','POST'])
 def category(cate):
     '''
     function to return the pitches by category
     '''
+
+
+    form = CommentForm()
+    
+    
     pitches = Pitches.query.filter_by(category=cate).all()
     print(category)
     title = f'{cate}'
-    return render_template('categories.html',title = title, pitches = pitches)
-
-@main.route('/user/<uname>')
-def profile(uname):
-    user = User.query.filter_by(author = uname).first()
-
-    if user is None:
-        abort(404)
-
-    return render_template("profile/profile.html", user = user)
+    return render_template('categories.html',title = title, pitches = pitches,comment_form = form)
 
 
-@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@main.route('/all_comment/<int:pitches_id>', methods = ['GET', 'POST'])
 @login_required
-def update_profile(uname):
-    user = User.query.filter_by(author = uname).first()
-    if user is None:
-        abort(404)
+def all_comment(pitches_id):
+    pitch = Pitches.query.filter_by(id=pitches_id).first()
+    comments = Comments.query.filter_by(pitches_id=pitches_id).all()
 
-    form = UpdateProfile()
-
-    if form.validate_on_submit():
-        user.bio = form.bio.data
-
-        
-
-        return redirect(url_for('.profile',uname=user.author))
-
-    return render_template('profile/update.html',form =form)
-
-
-@main.route('/comments/<id>')
-@login_required
-def comment(id):
-    '''
-    function to return the comments
-    '''
-    comm =Comments.get_comment(id)
-    print(comm)
-    title = 'comments'
-    return render_template('comments.html',comment = comm,title = title)
-
-@main.route('/new_comment/<int:pitches_id>', methods = ['GET', 'POST'])
-@login_required
-def new_comment(pitches_id):
-    pitches = Pitches.query.filter_by(id = pitches_id).first()
     form = CommentForm()
-
     if form.validate_on_submit():
         comment = form.comment.data
 
-        new_comment = Comments(comment=comment,user_id=current_user.id, pitches_id=pitches_id)
+        new_comment = Comments(comment=comment,user=current_user,pitch=pitch)
 
 
-        new_comment.save_comment()
+        db.session.add(new_comment)
+        db.session.commit()
+
+       
+
+        return redirect(url_for('categories.html',cate =pitch.category))
 
 
-        return redirect(url_for('main.index'))
-    title='New Pitch'
-    return render_template('new_comment.html',title=title,comment_form = form,pitches_id=pitches_id)
+    
+    return render_template('new_comment.html',comments= comments)
   
      
